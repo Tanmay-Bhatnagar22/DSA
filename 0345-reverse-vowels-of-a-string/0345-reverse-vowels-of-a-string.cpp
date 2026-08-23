a3daae69b681f479a1ad84c18e00b350bda91186
Tanmay Bhatnagar <tanmay.24bcy10155@vitbhopal.ac.in>
2026-08-23 16:22:24 +0530

    Time: 4 ms (18.92%), Space: 10.3 MB (73.66%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        auto isVowel = [](char c) {
+            c = tolower(c);
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        };
+        
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (!isVowel(s[left])) {
+                left++;
+            } else if (!isVowel(s[right])) {
+                right--;
+            } else {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+};
