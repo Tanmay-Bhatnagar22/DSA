b0e1a17811d896e0bad7e131c19751ee4031545d
Tanmay Bhatnagar <tanmay.24bcy10155@vitbhopal.ac.in>
2026-09-03 14:23:40 +0530

    Time: 0 ms (100%), Space: 10.8 MB (74.31%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int altitude = 0, best = 0;
+        for (int g : gain) {
+            altitude += g;
+            best = max(best, altitude);
+        }
+        return best;
+    }
+};
